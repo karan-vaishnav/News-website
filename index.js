62e288ae3eb57b88299aefb8f56b561e958a0684
Karan Vaishnav <85957167+karan-vaishnav@users.noreply.github.com>
2021-12-09 11:31:12 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,38 @@
+// grab the news container
+let newsAccordion = document.getElementById('newsAccordion');
+
+//creat an ajax get request
+const xhr = new XMLHttpRequest();
+xhr.open('GET', 'https://newsapi.org/v2/top-headlines?country=in&apiKey=07b6f945ea5d4f3d927418fe3b8f2aed', true);
+// What to do when response is ready
+xhr.onload = function () {
+    if (this.status === 200) {
+
+        let json = JSON.parse(this.responseText);
+        let articles = json.articles;
+        //console.log(articles);
+        let newsHtml = '';
+        articles.forEach(function(element, index) {
+            //console.log(element, index)
+            let news = `<div class="accordion-item">
+                            <h2 class="accordion-header" id="heading${index}">
+                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse${index}"
+                                    aria-expanded="true" aria-controls="collapse${index}">
+                                    <b>Breaking News ${index+1}: </b>${element["title"]}
+                                </button>
+                            </h2>
+                            <div id="collapse${index}" class="accordion-collapse collapse" aria-labelledby="heading${index}"
+                                data-bs-parent="#newsAccordion">
+                                <div class="accordion-body"> ${element["content"]}. <a href="${element['url']}" target="_blank" >Read more here</a> </div>
+                            </div>
+                        </div> `;
+            newsHtml += news;
+        });
+        newsAccordion.innerHTML = newsHtml
+    }
+    else {
+        console.log("Some error occured")
+    }
+}
+
+xhr.send()
